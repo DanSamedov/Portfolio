83a878c41973e58e14fda07ba8a53cfdf8b73dc1
DanSamedov <danasamedov@gmail.com>
2025-12-10 22:29:26 +0100

    Add email sending

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -4,5 +4,4 @@ from .models import ContactMessage
 class ContactMessageSerializer(serializers.ModelSerializer):
     class Meta:
         model = ContactMessage
-        # These are the fields the frontend is allowed to send
         fields = ['name', 'email', 'message']
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,12 +1,24 @@
+# backend/api/views.py
 from rest_framework import generics
-from rest_framework.throttling import ScopedRateThrottle
+from django.conf import settings
 from .models import ContactMessage
 from .serializers import ContactMessageSerializer
+from .utils import EmailThread 
 
 class ContactCreateView(generics.CreateAPIView):
     queryset = ContactMessage.objects.all()
     serializer_class = ContactMessageSerializer
     
-    throttle_classes = [ScopedRateThrottle]
-    
-    throttle_scope = 'contact_email'
+    def perform_create(self, serializer):
+        instance = serializer.save()
+
+        subject = f"Thanks for reaching out, {instance.name}!"
+        message = f"Hi {instance.name} :),\n\nI received your message and will get back to you as fast as possible.\n\nBest regards,\nDanylo Samedov"
+        recipient_list = [instance.email]
+
+        EmailThread(subject, message, recipient_list).start()
+        EmailThread(
+            subject=f"New Contact: {instance.name}",
+            message=f"Name: {instance.name}\nEmail: {instance.email}\n\nMessage:\n{instance.message}",
+            recipient_list=[settings.EMAIL_HOST_USER]
+        ).start()
